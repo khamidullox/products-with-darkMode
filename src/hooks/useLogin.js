238219fa3b3f140b8d2edfc29bb38d70c2939597
khamidullox <hamidullo22022@gmail.com>
2024-05-01 14:52:11 +0500

    useEffect

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,23 +1,29 @@
 import { useActionData } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useContext } from "react";
+import { useContext , useEffect } from "react";
 import { GlobalContext } from "../context/useGlobal";
 function useLogin() {
   let { dispetch } = useContext(GlobalContext);
   let actionData = useActionData();
+  useEffect(() => {
+    if (actionData) {
+      handleLogin()
+    }})
   let handleLogin = () => {
     const auth = getAuth();
-    signInWithEmailAndPassword(auth, actionData.email, actionData.password)
-      .then((userCredential) => {
-        const user = userCredential.user;
+    if (actionData) {
+      signInWithEmailAndPassword(auth, actionData.email, actionData.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
 
-        dispetch({ type: "LOG_IN", paylod: user });
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-      });
+          dispetch({ type: "LOG_IN", paylod: user });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage);
+        });
+    }
   };
   return { handleLogin };
 }
